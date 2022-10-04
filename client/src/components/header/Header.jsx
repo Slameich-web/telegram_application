7fe3946e380c63dbe57349afdcc4f7eae0e5423f
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-05 01:31:24 +0300

    template

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import { Button } from "../button/Button";
 import "./Header.css";
 export const Header = () => {
   const { onClose, user } = useTelegram();
+  const [state, setState] = useState("");
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={"username"}>{user.username}</span>
+      Привет!
+      <span className={"username"}>
+        <strong>{user.username}</strong>
+      </span>
+      <h1>Добро пожаловать в мое портфолио</h1>
+      <div>
+        Тут вы сможете найти описание моих пет проектов и ссылки на них{" "}
+        <Button onClick={() => setState("pet")}>Закрыть</Button>
+      </div>
+      <div>
+        Также здесь вы сможете узнать над какими коммерческими задачами я
+        работал <Button onClick={() => setState("prod")}>Закрыть</Button>
+      </div>
+      {state === "pet" ? <div>Пет проекты</div> : null}
     </div>
   );
 };
