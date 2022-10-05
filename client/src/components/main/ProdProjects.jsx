cc2738626058318aa726e92cb1aeba4384d7e849
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-05 13:11:45 +0300

    fix

--- client/src/components/main/ProdProjects.jsx
+++ client/src/components/main/ProdProjects.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const ProdProjects = () => {
   return (
     <div>
-      <h3>Ru LinkedIn</h3>
+      <h3>MTS BANK Portal</h3>
       <div>Большой портал для сотрудников МТС Банка</div>
     </div>
   );
